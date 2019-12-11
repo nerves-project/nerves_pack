d2e2fb822a9223c117fdb41c682940d1c9d99590
Justin Schneck <jschneck@mac.com>
2019-12-11 12:50:07 -0500

    Add circle ci config

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule NervesPack.MixProject do
     [
       app: :nerves_pack,
       version: @version,
-      elixir: "~> 1.9",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       docs: docs(),
