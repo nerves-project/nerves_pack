5284c47e5ae57a2fae3ed207a81932f8631b93e4
Frank Hunleth <fhunleth@troodon-software.com>
2020-06-01 09:45:36 -0400

    Add changelog to hexdocs

--- mix.exs
+++ mix.exs
@@ -51,10 +51,11 @@ defmodule NervesPack.MixProject do
 
   defp docs do
     [
-      extras: ["README.md"],
+      extras: ["README.md", "CHANGELOG.md"],
       main: "readme",
       source_ref: "v#{@version}",
-      source_url: @source_url
+      source_url: @source_url,
+      skip_undefined_reference_warnings_on: ["CHANGELOG.md"]
     ]
   end
 
